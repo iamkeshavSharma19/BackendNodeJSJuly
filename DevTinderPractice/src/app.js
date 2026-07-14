1182e6c93bebc23419a4ca9dd07aa61eacb44a34
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-07-14 19:53:35 +0530

    ExpressJS Diving Deep Into The API'S

--- DevTinderPractice/src/app.js
+++ DevTinderPractice/src/app.js
@@ -7,24 +7,52 @@ import { User } from "./models/user.TillMongooseModelsAndSchema.js";
 const app = express();
 const PORT = process.env.PORT || 8888;
 
-app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Ganesh",
-    lastName: "Sharma",
-    emailId: "Ganesh123@gmail.com",
-    password: "Ganesh@123",
-    designation: "Software Engineer",
-  });
+//&This middleware is used for parsing the JSON data into the simple JS object
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
     await user.save();
     res.status(201).json({
-      message: "User created successfully",
+      message: "User signed up successfully",
       data: user,
     });
   } catch (error) {
-    res.status(400).json({
-      message: "Error saving the user",
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+  }
+});
+
+//?Reading the data from the database
+//*Creating an api to find only one user from the database
+//*GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  console.log(userEmail);
+
+  try {
+    //?find() method basically returns the array of objects that will be matched whereas findOne method will only return you the one object which would be matched.
+    const users = await User.find({
+      emailId: userEmail,
+    });
+
+    if (users.length === 0) {
+      res.status(400).json({
+        message: "User not found",
+      });
+    } else {
+      res.status(200).json({
+        message: "user found successfully",
+        data: users,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
       error,
     });
   }
