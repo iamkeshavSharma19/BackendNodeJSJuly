b8fe0cdb6b1ddc4ea574f99f7ae71d72c2a7334a
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-07-16 18:22:01 +0530

    book app ongoing

--- ExpressJSTask1MVC/src/controllers/book-controllers.js
+++ ExpressJSTask1MVC/src/controllers/book-controllers.js
@@ -24,11 +24,66 @@ export const handleCreateBook = async (req, res) => {
   }
 };
 
-export const handleGetSingleBook = async (req, res) => {};
+export const handleGetSingleBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    console.log(bookId);
+    const bookToBeFound = await Book.findOne({ _id: bookId });
+    console.log(bookToBeFound);
+    if (!bookToBeFound) {
+      res.status(404).json({
+        message: "Unable to find the book",
+        book: bookToBeFound,
+      });
+    } else {
+      res.status(200).json({
+        message: "Book Found",
+        data: bookToBeFound,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
 
-export const handleGetAllBooks = async (req, res) => {};
+export const handleGetAllBooks = async (req, res) => {
+  try {
+    const allBooks = await Book.find({});
+    if (allBooks.length === 0) {
+      return res.status(404).json({
+        message: "Unable To Find All The Books",
+        books: allBooks,
+      });
+    }
+    res.status(200).json({
+      message: "Fetched All The Books Successfully",
+      books: allBooks,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
 
-export const handleDeleteBook = async (req, res) => {};
+export const handleDeleteBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const bookToBeDeleted = Book.findByIdAndDelete({ _id: bookId });
+    if (!bookToBeDeleted) {
+      return res.status
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
 
 export const handleEditBook = async (req, res) => {};
 
