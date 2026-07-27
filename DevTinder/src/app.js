a82639fdb2d338e947af91100eec4db590d4e654
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-07-27 19:54:04 +0530

    Building Feed API And Pagination going Strong

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -17,6 +17,9 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+//&EP14 ==> In this episode we are going to talk about the most important api which is basically the feed API.Whenever the user logs In,So he basically sees the different types of cards.He sees the data of the other users onto the application and then he can connect or reject those people.This api will basically bring up the data of the other users.This API will get me the data of the Users and then I can left or right swipe accordingly on the UI.
+
+//~So let us just start now building the feed API.We will start building this feed api.This is again the api inside the userRouter.Let us just go to the userRouter and start creating our Feed API.
 
 
 app.use("/", authRouter);
--- DevTinder/src/routes/user.js
+++ DevTinder/src/routes/user.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userAuth } from "../middlewares/auth.js";
 import ConnectionRequest from "../models/connectionRequests.js";
+import { User } from "../models/user.js";
 
 const userRouter = express.Router();
 
@@ -83,6 +84,83 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       message: "Connection requests found successfully",
       connectionRequests: data,
     });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something Went Wrong",
+      error: error.message,
+    });
+  }
+});
+
+//?feed API
+//?Thought Process Of Writing the feed API ==> Which Users should be visible to the loggedIn User ??Let say loggedIn User is Virat.
+
+//? Virat should see the card of the users, Virat should see the card of everybody whom Virat has not sent connectionRequest to.Suppose If Virat has already sent a request to Elon,then Virat should not see again the card Of Elon.There is basically no point in showing the card of People whom Virat has already marked Interested.
+
+//*Now Suppose Virat has ignored somebody's profile.If Virat has ignored Donald Trump's profile.Should Virat see Donald Trump again on the feed??No.The people who has been went into the "interested",may be If you have sent the connection request to them or you have ignored their profile,You should not see them again in your feed.
+
+//* Virat should not see the Users who are his connections already,suppose Virat is already connected to Elon,so if they both are already friends,Why would you show the card of the connections again in the feed.There is no point in seeing it.
+
+//^There is one more condition that Virat should not see his Card himself,Suppose If You are fetching all the users,let say there are 6 users on the platform,Virat should not see his card himself.So you have to write a MongoDB query in such a way that you have to exclude all such people,whom we donot want in the feed.Now this logic is a little Complex to write.Let me meet you inside the try Block.
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    //?What are the cards that the user should not see??
+    //?1.)User's own card.
+    //?2.)His connections.
+    //?3.)Ignored People.
+    //?4.)People to whom I have already sent a connection Request.
+    //^ex => Virat, Elon, Mark, Harry, Ron.
+    //^Let say a new user Rahul comes onto our Platform.So what feed Rahul should see?Rahul should see = [Virat, Elon, Mark, Harry, Ron] except himself.
+    //~Now Suppose Rahul has sent the connection Request to Virat.So Rahul should see everyone except the person to whome he has sent the connection request.Rahul should see = [Elon, Mark, Harry, Ron].
+    //*Now let say Rahul has sent a connection request to Elon also.Now Rahul should see on his feed = [Mark, Harry, Ron].
+    //? Now let say Virat has rejected the connection request of the Rahul.Should Virat should again be shown to the Rahul?No Virat should not be shown again on the Rahul's Profile.
+    //&Now let say Elon has accepted the connectionReq of the Rahul.Should Rahul see Elon back into the feed.No
+    //?Basically If the entry has already been created inside the connectionRequest Collection,then those persons should not be able to see each other's Profile once again.
+    const loggedInUser = req.user;
+    //*Now I will find all the connectionRequests that either I[loggedInuser] have sent or I have received because If I have sent a request to somebody,so I should not see that on the feed,and If I have received somebody's request,then I should'not see them also on the feed.
+    //?firstOf all I donot need all that random data,I dont care about createdAt, status, updatedAt, _id.so basically there is a select function and you can select that what all data do you need from this data.I just need my fromUserId and toUserId.
+    //?Test in the postMan by making Harry LoggedIn.
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      //?But here in the select these fromUserId and toUserId also looks ugly, let me now make the use of the populate.[just to show case, I am going to remove populate just uncomment it]
+    }).select("fromUserId toUserId status");
+    // .populate("fromUserId", ["firstName", "lastName"])
+    // .populate("toUserId", ["firstName", "lastName"]);
+    //!Now just think about that I have to generate the feed of Harry Potter.Harry should not see Ron, Virat and Brock Lesnar.All the people whome you have sent or all the people whom you have received,you should'not see them in the feed.
+    //!Now these {fromUserId, toUserId} are the people whom I donot want to see in the feed.
+    //!So How will I segregate these people, so I will basically hide these users.For that I will create a data structure known as set.This Set data structure is like an array.Suppose If you are adding "A" in the set, it will add "A",Suppose If you are adding "B" in the data structure,it will add "B",Suppose If you tried to add a repeated value "A" inside it once again,so "A" will not be added in it because "A" is already present inside the Set.Set data structure will always contain the unique elements.
+    //!If you are sending the repeated values, it will just ignore it.
+    //?Once i have created the set,now I will loop through all these connection requests.
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    console.log(hideUsersFromFeed); //?In my set I have 4 unique people now from whom I have to hide the loggedIn User's feed.
+
+    //?Now for finding out the remaining users, I will basically write a reverse query kind of a thing.
+    const users = await User.find({
+      //?I will say find all the users with the id and that id should not be present in the ($nin) Array.from(hideUsersFeed).
+      $and: [
+        {
+          _id: {
+            //*Array.from() will convert the set into an array.
+
+            $nin: Array.from(hideUsersFromFeed),
+          },
+        },
+        {
+          //*second condition ==> I donot want all the people who are there in the hideUsersFromFeed, I also donot want my own card to be there, for this I also have to write a $and query over here.
+          _id: {
+            $ne: loggedInUser._id,
+          },
+        },
+      ],
+    });
+
+    res.send(connectionRequests);
   } catch (error) {
     res.status(500).json({
       message: "Something Went Wrong",
