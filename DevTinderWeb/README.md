ea2b9b2fc23f1677ec412342b658c093337afff0
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-07-28 17:35:14 +0530

    Basic React Set Up Done In The DevTinder

--- DevTinderWeb/README.md
+++ DevTinderWeb/README.md
@@ -6,4 +6,9 @@
 - Remove unnecessary code and create a Hello World App
 - Install Tailwind CSS
 - Install Daisy UI
-- Add Navbar component to App.jsx 
+- Add Navbar component to App.jsx
+- Create a Navbar separate Component File
+- Install react-router-dom
+- Create BrowserRouter > Routes > Route=/Body > RouteChildren
+- Create an Outlet in your Body Component.
+- Create a footer.
---
+++ DevTinderWeb/src/components/Body.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+const Body = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* for rendering Children Routes, Use Outlet, All the children routes will be rendered inside the <Outlet/> component */}
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
