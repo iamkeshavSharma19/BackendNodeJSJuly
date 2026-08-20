265d5fd5ed6736cdc0ed104f0f37e3a2460d45f3
Keshav Sharma <Keshav.bhambi@gmail.com>
2026-08-20 14:55:36 +0530

    Diving Deep Into The API's Completed, Practice Own My Own

--- DevTinderAugPractice/src/app.js
+++ DevTinderAugPractice/src/app.js
@@ -11,12 +11,24 @@ const app = express();
 app.use(express.json());
 
 //?Diving Deep Into The API's
+//?SignUp API
 app.post("/signup", async (req, res) => {
   try {
+    if (!req.body) {
+      return res.status(400).json({
+        message: "Unable to signUp The User",
+      });
+    }
+    const userData = Object.keys(req.body);
+    if (userData.length === 0) {
+      return res.status(400).json({
+        message: "Unable To SignUp The User",
+      });
+    }
     const user = new User(req.body);
     await user.save();
     res.status(201).json({
-      message: "User SignedUp Sucessfully onto our Database",
+      message: "User Signedup Successfully",
       user,
     });
   } catch (error) {
@@ -27,17 +39,16 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-//?Designing an API to get only one user from the database.GET user by email
+//?Designing an API to get only one user from the database.GET user by email.By Using find() Method.
 app.get("/user/:emailId", async (req, res) => {
   try {
     const { emailId: userEmail } = req.params;
-    //?find method basically returns you the array of all the matched documents.If there is no matched document then It returns an empty array
-    const users = await User.find({
-      emailId: userEmail,
-    });
+    //?find method returns the array of all the matched documents,if there are no matched documents then It basically returns the empty array
+    const users = await User.find({ emailId: userEmail });
     if (users.length === 0) {
       return res.status(404).json({
-        message: "No Users Found",
+        message: "User Not Found",
+        users,
       });
     }
     res.status(200).json({
@@ -55,23 +66,23 @@ app.get("/user/:emailId", async (req, res) => {
 //~findOne User.
 app.get("/findOneUser/:emailId", async (req, res) => {
   try {
-    //~findOne method returns you the single matched document only not the whole Array.If there is no matched document then It returns the null.
-    //~If you donot pass anything inside the findOne({}) then it returns the first document present inside the database.
     const { emailId: userEmail } = req.params;
+
     const user = await User.findOne({ emailId: userEmail });
+
     if (!user) {
       return res.status(404).json({
-        message: "User Not Found",
+        message: "Unable To Find The User",
         user,
       });
     }
     res.status(200).json({
-      message: "User Found",
+      message: "User Found Successfully",
       user,
     });
   } catch (error) {
     res.status(500).json({
-      message: "Something went wrong",
+      message: "Something Went Wrong",
       error: error.message,
     });
   }
@@ -83,12 +94,12 @@ app.get("/feed", async (req, res) => {
     const allUsers = await User.find({});
     if (allUsers.length === 0) {
       return res.status(404).json({
-        message: "Users Not Found",
+        message: "Unable to fetch All The Users",
         allUsers,
       });
     }
     res.status(200).json({
-      message: "Users Found",
+      message: "All The Users Are Fetched Successfully",
       allUsers,
     });
   } catch (error) {
@@ -103,11 +114,9 @@ app.get("/feed", async (req, res) => {
 app.delete("/user/delete/:id", async (req, res) => {
   try {
     const { id: deleteId } = req.params;
-    console.log(deleteId);
     const deletedUser = await User.findByIdAndDelete({ _id: deleteId });
-    console.log(deletedUser);
     if (!deletedUser) {
-      return res.status(400).json({
+      return res.status(404).json({
         message: "Unable To Delete The User",
         deletedUser,
       });
@@ -128,16 +137,21 @@ app.delete("/user/delete/:id", async (req, res) => {
 app.patch("/user/edit/:id", async (req, res) => {
   try {
     const { id: editId } = req.params;
-    const data = req.body;
-    const updatedUser = await User.findByIdAndUpdate({ _id: editId }, data, {
-      returnDocument: "after",
-    });
+    const updatedData = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id: editId },
+      updatedData,
+      {
+        returnDocument: "after",
+      },
+    );
     if (!updatedUser) {
       return res.status(400).json({
         message: "Unable To Edit The User",
         updatedUser,
       });
     }
+
     res.status(201).json({
       message: "User Updated Successfully",
       updatedUser,
